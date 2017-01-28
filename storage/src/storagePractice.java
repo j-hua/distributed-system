5ca5462cd46f4293d84e23e0cbf754fb6fc6cf63
Haque  Waref <haquewar@ug158.eecg>
2017-01-28 13:25:39 -0500

    merged

--- storage/src/storagePractice.java
+++ storage/src/storagePractice.java
@@ -5,13 +5,30 @@ import java.util.List;
 
 public class storagePractice {
 
-	//this is going to be FIFO
+	//this is going to be used for all replacement policies
 	List<String> keyCache = new ArrayList<String>();
+	List<String> valueCache = new ArrayList<String>();
+	
 	static int limit = 10;
+	static String replacementPolicy = "LRU";
 	
 	public static void main(String[] args) {
 		storagePractice test = new storagePractice();
-		System.out.println(test.get("lol you suck"));
+		System.out.println(test.put("one","52"));
+		System.out.println(test.put("two","52"));
+		System.out.println(test.put("three","52"));
+		System.out.println(test.put("four","52"));
+		System.out.println(test.put("five","52"));
+		System.out.println(test.put("six","52"));
+		System.out.println(test.put("seven","52"));
+		System.out.println(test.put("eight","52"));
+		System.out.println(test.put("nine","52"));
+		System.out.println(test.put("ten","52"));
+		System.out.println(test.put("one","54"));
+		System.out.println(test.put("boy", "this is a long value"));
+		System.out.println(test.get("boy"));
+		System.out.println(test.get("two"));
+		System.out.println(test.put("boy", null));
 	}
 
 	public String put(String key, String value){
@@ -35,11 +52,11 @@ public class storagePractice {
 				
 				while((line = br.readLine()) != null){
 					if(line.length() != 0){
-						String[] kv = line.split(",");
+						String[] kv = line.split(" ");
 						if(kv[0].equals(key)){
 							//replace this line
 							replaced = true;
-							printTemp.println(key+","+value);
+							printTemp.println(key+" "+value);
 						}else{
 							printTemp.println(line);
 						}
@@ -53,18 +70,8 @@ public class storagePractice {
 					//delete temp file
 					System.out.println("Temp Deletion: " + temp.delete());
 					
-					printWrite.println(key+","+value);
+					printWrite.println(key+" "+value);
 					printWrite.close();
-					
-					//add key to cache if not already there
-					if(!keyCache.contains(key)){
-						//not there, add to list
-						//check if array is full
-						if(keyCache.size() == limit){
-							//remove the first element by virtue of FIFO
-							
-						}
-					}
 					
 					status = "PUT SUCCESSFUL";
 				} else {
@@ -74,6 +81,54 @@ public class storagePractice {
 					status = "PUT UPDATE SUCCESSFUL";
 				}
 				
+				if(replacementPolicy.equals("FIFO")){
+					//FIFO CACHE----------------------------------------------------------------------------
+					//add key to cache if not already there
+					if(!keyCache.contains(key)){
+						//not there, add to list
+						//check if array is full
+						if(keyCache.size() == limit){
+							//remove the first element by virtue of FIFO
+							System.out.println("Key: " + keyCache.get(0) + " Value: " + valueCache.get(0) + " REMOVED FROM CACHE");
+							keyCache.remove(0);
+							valueCache.remove(0);
+						}
+						System.out.println("Key: " + key + " Value: " + value + " ADDED TO CACHE");
+						keyCache.add(key);
+						valueCache.add(value);
+					}
+					//---------------------------------------------------------------------------------------
+				} else if (replacementPolicy.equals("LRU")){
+					//LRU CACHE----------------------------------------------------------------------------
+					//add key to cache if not already there
+					if(!keyCache.contains(key)){
+						//not there, add to end of list, indicates most recently used
+						//check if array is full
+						if(keyCache.size() == limit){
+							//remove the first element by virtue of LRU (first is the least recently used)
+							System.out.println("Key: " + keyCache.get(0) + " Value: " + valueCache.get(0) + " REMOVED FROM CACHE");
+							keyCache.remove(0);
+							valueCache.remove(0);
+						}
+						System.out.println("Key: " + key + " Value: " + value + " ADDED TO CACHE");
+						keyCache.add(key);
+						valueCache.add(value);
+					} else {
+						//the key cache contains the key, thus a put update happened, remove key and value from cache
+						System.out.println("Removing " + keyCache.get(keyCache.indexOf(key)) + " and " + valueCache.get(keyCache.indexOf(key)));
+						valueCache.remove(keyCache.indexOf(key));
+						keyCache.remove(keyCache.indexOf(key));
+						
+						//add to end of list, that means it was the most recently used
+						keyCache.add(key);
+						valueCache.add(value);
+						
+						System.out.println(key + " " + value + " added to end of list");
+						System.out.println(keyCache.toString());
+						System.out.println(valueCache.toString());
+					}
+					//---------------------------------------------------------------------------------------
+				}
 				br.close();
 				
 			} else {
@@ -86,7 +141,7 @@ public class storagePractice {
 				
 				while((line = br.readLine()) != null){
 					if(line.length() != 0){
-						String[] kv = line.split(",");
+						String[] kv = line.split(" ");
 						if(kv[0].equals(key)){
 							//delete this line
 							deleted = true;
@@ -110,6 +165,20 @@ public class storagePractice {
 					System.out.println("Original Deletion: " + inputFile.delete());
 					System.out.println("Renaming of Temp: " + temp.renameTo(inputFile));
 					status = "KEY DELETE SUCCESSFUL";
+					
+					if(replacementPolicy.equals("FIFO") || replacementPolicy.equals("LRU")){
+						//FIFO OR LRU--------------------------------------------------------
+						//If in cache, remove it
+						if(keyCache.contains(key)){
+							System.out.println("Removing " + keyCache.get(keyCache.indexOf(key)) + " and " + valueCache.get(keyCache.indexOf(key)));
+							valueCache.remove(keyCache.indexOf(key));
+							keyCache.remove(keyCache.indexOf(key));
+							
+							System.out.println(keyCache.toString());
+							System.out.println(valueCache.toString());
+						}
+						//-------------------------------------------------------------------
+					}
 				}
 				
 				br.close();
@@ -131,15 +200,84 @@ public class storagePractice {
 			
 			String line;
 			
+			if(replacementPolicy.equals("FIFO")){
+				//FIFO--------------------------------------------------------------------
+				//First check if it is in the cache
+				if(keyCache.contains(key)){
+					System.out.println("KEY FOUND IN CACHE");
+					br.close();
+					return valueCache.get(keyCache.indexOf(key));
+				}
+				//------------------------------------------------------------------------
+			} else if (replacementPolicy.equals("LRU")){
+				//LRU---------------------------------------------------------------------
+				//First check if it is in the cache
+				if(keyCache.contains(key)){
+					System.out.println("KEY FOUND IN CACHE");
+					String value = valueCache.get(keyCache.indexOf(key));
+					
+					//the key cache contains the key, thus a put update happened, remove key and value from cache
+					System.out.println("Removing " + keyCache.get(keyCache.indexOf(key)) + " and " + valueCache.get(keyCache.indexOf(key)));
+					valueCache.remove(keyCache.indexOf(key));
+					keyCache.remove(keyCache.indexOf(key));
+					
+					//add to end of list, that means it was the most recently used
+					keyCache.add(key);
+					valueCache.add(value);
+					
+					System.out.println(key + " " + value + " added to end of list");
+					System.out.println(keyCache.toString());
+					System.out.println(valueCache.toString());
+					
+					br.close();
+					return value;
+				}
+				//------------------------------------------------------------------------
+			}
+			
 			while((line = br.readLine()) != null){
 				if(line.length() != 0){
-					String[] kv = line.split(",");
+					String[] kv = line.split(" ");
 					if(kv[0].equals(key)){
-						return kv[1];
+						//skip the key and the first part of the value because its already been stored in value
+						boolean skipFirst = false;
+						boolean skipSecond = false;
+						String value = kv[1];
+						
+						//concatenate all the other parts of the value
+						for (String part : kv){
+							if(!skipFirst){
+								skipFirst = true;
+							} else if(!skipSecond){
+								skipSecond = true;
+							} else{
+								value = value + " " + part;
+							}
+						}
+						br.close();
+						
+						if(replacementPolicy.equals("FIFO") || replacementPolicy.equals("LRU")){
+							//FIFO or LRU--------------------------------------------------
+							//Add to cache because it is not there
+							//check if array is full
+							if(keyCache.size() == limit){
+								//remove the first element by virtue of FIFO
+								//or remove by virtue of being the least recently used (LRU)
+								System.out.println("Key: " + keyCache.get(0) + " Value: " + valueCache.get(0) + " REMOVED FROM CACHE");
+								keyCache.remove(0);
+								valueCache.remove(0);
+							}
+							System.out.println("Key: " + key + " Value: " + value + " ADDED TO CACHE");
+							keyCache.add(key);
+							valueCache.add(value);
+							//------------------------------------------------------------
+						}
+						
+						return value;
 					}
 				}
 			}
-			
+			br.close();
 			return "GET ERROR: KEY NOT FOUND";
 		} catch (IOException e) {
 			e.printStackTrace();
