b127b9181a7a0e4cba3ed770ca2f5a49ebd9230f
J.HUA <jeffrey.hua@hotmail.com>
2017-01-29 14:02:48 -0500

    edited additional test

--- M1/BasicStorageServer-stub/src/testing/AdditionalTest.java
+++ M1/BasicStorageServer-stub/src/testing/AdditionalTest.java
@@ -3,6 +3,7 @@ package testing;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+//import KVServer.*;
 
 public class AdditionalTest extends TestCase {
 	
@@ -12,4 +13,31 @@ public class AdditionalTest extends TestCase {
 	public void testStub() {
 		assertTrue(true);
 	}
+
+/*
+	@Test
+	public void getCheck(){
+		KVServer kvServer = new KVServer(PORT,CACHE_SIZE,CACHE_STRATEGY);
+		Exception putEx=null;
+		Exception getEx = null;
+		KVMessage kvMessage = null;
+		KVMessage getMessage = null;
+
+		try {
+			kvMessage = kvServer.put("one","52");
+		} catch (Exception e) {
+			putEx = e;
+		}
+
+		assertTrue(putEx == null && kvMessage.getStatus() == KVMessage.StatusType.PUT_SUCCESS);
+		try {
+			getMessage = kvServer.get("one");
+		} catch (Exception e) {
+			getEx = e;
+		}
+
+		assertTrue(getEx == null && getMessage.getStatus() == KVMessage.StatusType.GET_SUCCESS);
+
+	}*/
 }
+
