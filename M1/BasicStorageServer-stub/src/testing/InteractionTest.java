679c2247bebb2df76796fbe83340c85169d49847
Haque  Waref <haquewar@ug158.eecg>
2017-01-29 16:09:43 -0500

    passed 9 test cases, need to pass testPUT

--- M1/BasicStorageServer-stub/src/testing/InteractionTest.java
+++ M1/BasicStorageServer-stub/src/testing/InteractionTest.java
@@ -120,7 +120,7 @@ public class InteractionTest extends TestCase {
 
 	@Test
 	public void testGetUnsetValue() {
-		String key = "an unset value";
+		String key = "anunsetvalue";
 		KVMessage response = null;
 		Exception ex = null;
 
